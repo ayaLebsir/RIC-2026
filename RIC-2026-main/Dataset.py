51d5c4b4ef7159b9fc35f61000ee3de0e0cce730
ayaLebsir <lebsiraya3@gmail.com>
2026-04-02 14:33:27 +0100

    step 1:Generate random points

--- RIC-2026-main/Dataset.py
+++ RIC-2026-main/Dataset.py
@@ -1,2 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# --- 1: توليد النقط العشوائية ---
+# هنا نولد 2000 نقطة عشوائية بين -5 و 5
+points = np.random.uniform(-5, 5, (2000, 2))
+
+
+x = points[:, 0]
+y = points[:, 1]
